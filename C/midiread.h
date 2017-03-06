12e9419a3c5963f5f54b99932faa8c63872d682c
Lagarde <sebastien.lagarde64@wanadoo.fr>
2017-03-06 22:50:52 +0100

    Update and rename findcomand.c to midiread.h

---
+++ C/midiread.h
@@ -0,0 +1,39 @@
+
+#ifndef MIDI_H
+#define MIDI_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**midi_opener.c**/
+
+
+void print_data(int d[2]);
+int read_midi(char* nom_fichier_midi);
+
+
+/**calc_utilit**/
+
+
+int read_cmd(char* s,int l,FILE* F,char* r);
+int cmpr(char* cmd, FILE * F);
+int time_calc(char s[4]);
+void find_note(int n);
+unsigned int conv(char* s);
+void clear(char* p,int l);
+
+
+/**analysis_midi**/
+void analyse (void);
+extern unsigned int bn;
+#ifdef __cplusplus
+}
+#endif
+#endif /*MIDI_H*/
