a19a4a21d96268a28e0c93543cf31ef6cdb03434
Lagarde <sebastien.lagarde64@wanadoo.fr>
2017-03-14 11:54:46 +0100

    Update fsm.vhd

--- vhd/fsm.vhd
+++ vhd/fsm.vhd
@@ -5,14 +5,22 @@ ENTITY fsm_midi IS
   PORT
     (
      MIDI_data: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-     CLK:IN STD_LOGIC;
+     
+     VLQ_CALC: IN STD_LOGIC_VECTOR (32 DOWNTO 0);
+     VLQ_DATA: OUT STD_LOGIC_VECTOR (32 DOWNTO 0);
+     
+     COMP_CALC: IN STD_LOGIC;
+     VAL_COMP_A: OUT STD_LOGIC_VECTOR (32 DOWNTO 0);
+     VAL_COMP_B: OUT STD_LOGIC_VECTOR (32 DOWNTO 0);
+     
+     NOTES_PLAYS: STD_LOGIC_VECTOR (32 DOWNTO 0);
+     CLK STD_LOGIC;
      CONV_data: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
     )
 END ENTITY fsm_midi;
   
-ARCHITECTURE fsm_midi_arch of fsm_midi IS
-  
-COMPONENT compar_data_cmd IS
+ 
+ COMPONENT compar_data_cmd IS
 -- composant comparant la valeur numérique de la donée midi entrante MIDI_c_data (buffer de taille 8) avec value_c
 --lenght_rd indique la longeur à lire
 --str_prt indique la ou doit commencer la lecture
@@ -31,6 +39,10 @@ COMPONENT time_vlq IS
         res_t: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
        )
 END COMPONENT time_vlq;
+   
+ARCHITECTURE fsm_midi_arch of fsm_midi IS
+  
+
     
 
     
